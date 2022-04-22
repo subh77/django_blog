3ee64293b75a22b9eb3a902e9b560d00134b9c08
Subhro <subhrodeep.sarkar@orchids.edu.in>
2022-04-22 10:30:53 +0530

    Blog models update

--- main/blog/models.py
+++ main/blog/models.py
@@ -1,3 +1,7 @@
+from pyexpat import model
+from tkinter import CASCADE
+from turtle import title
+from typing import cast
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,6 +12,13 @@ role_selection = (
     (1, "admin"),
     (2, "moderator"),
     (3, "gn_user"),
+)
+
+
+status_options = (
+    (1, "active"),
+    (2, "archived"),
+    (3, "deleted"),
 )
 
 
@@ -24,3 +35,10 @@ class ManagementUser(models.Model):
     ph_no = models.CharField(
         validators=phone_validation(), max_length=15, blank=False, null=True
     )
+
+
+class Blog(models.Model):
+    author = models.ForeignKey(User, on_delete=CASCADE, null=True)
+    title = models.CharField(max_length=200, null=True, blank=False)
+    content = models.TextField(null=True, blank=True, default="")
+    status = models.PositiveIntegerField(choices=status_options, blank=True, null=True)
